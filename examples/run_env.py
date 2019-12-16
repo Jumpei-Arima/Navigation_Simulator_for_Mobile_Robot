d1981ce27e3fb11cb5342cb77e7a458c3f29bcf6
arima <arijun0307@gmail.com>
2019-12-16 18:57:12 +0900

    added argparse

--- examples/run_env.py
+++ examples/run_env.py
@@ -1,21 +1,31 @@
+import argparse
+
 import numpy as np
 import gym
+
 from nsmr.envs import NsmrGymEnv
 
-print("Navigation Simulator for Mobile Robot")
-env = NsmrGymEnv(layout="simple_map")
+if __name__ == "__main__":
+    print("Navigation Simulator for Mobile Robot")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--layout', type=str, default='simple_map')
+    parser.add_argument('--max_episodes', type=int, default=100)
+    parser.add_argument('--max_steps', type=int, default=1000)
+    args = parser.parse_args()
 
-for i_episode in range(100):
-    observation = env.reset()
-    done = False
-    ep_r = 0
-    for t in range(1000):
-        env.render()
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        # print(observation)
-        # print(reward)
-        ep_r += reward
-        if done:
-            print("Episode %d  finished after %d timesteps, reward: %f "% (i_episode+1, t+1,ep_r))
-            break
+    env = NsmrGymEnv(layout=args.layout)
+
+    for i_episode in range(args.max_episodes):
+        observation = env.reset()
+        done = False
+        ep_r = 0
+        for t in range(args.max_steps):
+            env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            # print(observation)
+            # print(reward)
+            ep_r += reward
+            if done:
+                print("Episode %d  finished after %d timesteps, reward: %f "% (i_episode+1, t+1,ep_r))
+                break
