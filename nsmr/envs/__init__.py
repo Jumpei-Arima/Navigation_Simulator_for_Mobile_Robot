f919e949349df6b2ee5af66f903a18a2c55db5dd
Jumpei Arima <arijun0307@gmail.com>
2020-01-05 22:37:07 +0900

    update

--- nsmr/envs/__init__.py
+++ nsmr/envs/__init__.py
@@ -2,7 +2,6 @@ from gym.envs.registration import register
 
 from nsmr.envs.NsmrGymEnv import NsmrGymEnv
 from nsmr.envs.consts import *
-from nsmr.envs.layouts import *
 
 register(
     id='nsmr-v0',
