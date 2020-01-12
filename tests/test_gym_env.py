f34ea9bfc1f27cf80c831d3b19c4c80969ae84d8
Jumpei Arima <arijun0307@gmail.com>
2020-01-12 13:21:44 +0900

    update tests

--- tests/test_gym_env.py
+++ tests/test_gym_env.py
@@ -1,11 +1,31 @@
 from unittest import TestCase
 import gym
 import nsmr
+from nsmr.envs import NsmrGymEnv
+
 print(__file__)
 
 class TestNsmrGymEnv(TestCase):
-    def test_main1(self):
+    def test_main(self):
         env = gym.make("nsmr-v0")
+        obs = env.reset()
+
+        # Try stepping a few times
+        for i in range(10):
+            action = env.action_space.sample()
+            obs, reward, done, info = env.step(action)
+
+    def test_randomize(self):
+        env = NsmrGymEnv(randomize=True)
+        obs = env.reset()
+
+        # Try stepping a few times
+        for i in range(10):
+            action = env.action_space.sample()
+            obs, reward, done, info = env.step(action)
+
+    def test_render(self):
+        env = NsmrGymEnv(randomize=True)
         obs = env.reset()
 
         # Try stepping a few times
@@ -15,4 +35,6 @@ class TestNsmrGymEnv(TestCase):
 
 if __name__ == '__main__':  # pragma: no cover
     test = TestNsmrGymEnv()
-    test.test_main1()
+    test.test_main()
+    test.test_randomize()
+    test.test_render()
