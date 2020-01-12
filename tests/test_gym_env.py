ff9ebde4578a1e95338816a394085a9415e8a5eb
Jumpei Arima <arijun0307@gmail.com>
2020-01-12 16:15:44 +0900

    update tests

--- tests/test_gym_env.py
+++ tests/test_gym_env.py
@@ -1,4 +1,7 @@
 from unittest import TestCase
+import os
+import shutil
+import gym
 import nsmr
 from nsmr.envs import NsmrGymEnv
 
@@ -6,13 +9,14 @@ print(__file__)
 
 class TestNsmrGymEnv(TestCase):
     def test_main(self):
-        env = NsmrGymEnv()
+        env = gym.make("nsmr-v0")
         obs = env.reset()
 
         # Try stepping a few times
         for i in range(10):
             action = env.action_space.sample()
             obs, reward, done, info = env.step(action)
+        env.close()
 
     def test_randomize(self):
         env = NsmrGymEnv(randomize=True)
@@ -22,9 +26,11 @@ class TestNsmrGymEnv(TestCase):
         for i in range(10):
             action = env.action_space.sample()
             obs, reward, done, info = env.step(action)
+        env.close()
 
     def test_render(self):
-        env = NsmrGymEnv(randomize=True)
+        env = gym.make("nsmr-v0")
+        env = NsmrGymEnv()
         obs = env.reset()
 
         # Try stepping a few times
@@ -32,9 +38,24 @@ class TestNsmrGymEnv(TestCase):
             env.render()
             action = env.action_space.sample()
             obs, reward, done, info = env.step(action)
+        env.close()
+
+    def test_make_collision_map(self):
+        shutil.copyfile("./tests/test_map.json", "./nsmr/envs/layouts/test_map.json")
+        env = NsmrGymEnv(layout="test_map")
+        obs = env.reset()
+
+        # Try stepping a few times
+        for i in range(10):
+            action = env.action_space.sample()
+            obs, reward, done, info = env.step(action)
+        env.close()
+        os.remove("./nsmr/envs/layouts/test_map.json")
+        os.remove("./nsmr/envs/layouts/test_map_collision_map.pkl")
 
 if __name__ == '__main__':  # pragma: no cover
     test = TestNsmrGymEnv()
     test.test_main()
     test.test_randomize()
     test.test_render()
+    test.test_make_collision_map()
