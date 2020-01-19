ac81691f15650b9b4ca797b0771676f415f4b4fe
Jumpei Arima <arijun0307@gmail.com>
2020-01-19 21:38:01 +0900

    update test

--- tests/test_gym_env.py
+++ tests/test_gym_env.py
@@ -11,7 +11,6 @@ class TestNsmrGymEnv(TestCase):
     def test_main(self):
         env = gym.make("nsmr-v0")
         obs = env.reset()
-
         # Try stepping a few times
         for i in range(10):
             action = env.action_space.sample()
@@ -19,9 +18,9 @@ class TestNsmrGymEnv(TestCase):
         env.close()
 
     def test_randomize(self):
-        env = NsmrGymEnv(randomize=True)
+        env = gym.make("nsmr-v0")
+        env.set_randomimze(True)
         obs = env.reset()
-
         # Try stepping a few times
         for i in range(10):
             action = env.action_space.sample()
@@ -30,9 +29,7 @@ class TestNsmrGymEnv(TestCase):
 
     def test_render(self):
         env = gym.make("nsmr-v0")
-        env = NsmrGymEnv()
         obs = env.reset()
-
         # Try stepping a few times
         for i in range(10):
             env.render()
@@ -40,9 +37,25 @@ class TestNsmrGymEnv(TestCase):
             obs, reward, done, info = env.step(action)
         env.close()
 
+    def test_reward_params(self):
+        env = gym.make("nsmr-v0")
+        params={"goal_reward": 5.0,
+                "collision_penalty": 5.0,
+                "alpha": 0.3,
+                "beta": 0.01,
+                "stop_penalty": 0.05}
+        env.set_reward_params(params)
+        obs = env.reset()
+        # Try stepping a few times
+        for i in range(10):
+            action = env.action_space.sample()
+            obs, reward, done, info = env.step(action)
+        env.close()
+
     def test_make_collision_map(self):
         shutil.copyfile("./tests/test_map.json", "./nsmr/envs/layouts/test_map.json")
-        env = NsmrGymEnv(layout="test_map")
+        env = gym.make("nsmr-v0")
+        env.set_layout("test_map")
         env.close()
         os.remove("./nsmr/envs/layouts/test_map.json")
         os.remove("./nsmr/envs/layouts/test_map_collision_map.pkl")
@@ -52,4 +65,5 @@ if __name__ == '__main__':  # pragma: no cover
     test.test_main()
     test.test_randomize()
     test.test_render()
+    test.test_reward_params()
     test.test_make_collision_map()
