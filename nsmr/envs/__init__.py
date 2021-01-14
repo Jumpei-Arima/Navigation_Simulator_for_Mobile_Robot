7e240551db50a827d69c622cbf45f22cb1ea5160
Jumpei Arima <arijun0307@gmail.com>
2021-01-14 12:07:11 +0900

    update env_id and max_episode_steps

--- nsmr/envs/__init__.py
+++ nsmr/envs/__init__.py
@@ -6,19 +6,19 @@ from nsmr.envs.NsmrMinPoolingGymEnv import NsmrMinPoolingGymEnv
 from nsmr.envs.consts import *
 
 register(
-    id='nsmr-v0',
+    id='Nsmr-v1',
     entry_point='nsmr.envs.NsmrGymEnv:NsmrGymEnv',
-    max_episode_steps=1000,
+    max_episode_steps=500,
 )
 
 register(
-    id='nsmr-v1',
+    id='NsmrMinPooling-v1',
     entry_point='nsmr.envs.NsmrMinPoolingGymEnv:NsmrMinPoolingGymEnv',
-    max_episode_steps=1000,
+    max_episode_steps=500,
 )
 
 register(
-    id='NsmrSimple-v0',
+    id='NsmrSimple-v1',
     entry_point='nsmr.envs.NsmrSimpleGymEnv:NsmrSimpleGymEnv',
-    max_episode_steps=1000,
+    max_episode_steps=500,
 )
